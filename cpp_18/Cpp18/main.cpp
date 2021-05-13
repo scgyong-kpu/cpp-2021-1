55174a797194413895d36a53662f7b51465f6c07
Kiyong Kim <scgyong@kpu.ac.kr>
2021-05-13 16:09:15 +0900

    Image Resource 추가하고 spBg (outerspace.png) 화면에 표시

--- cpp_18/Cpp18/main.cpp
+++ cpp_18/Cpp18/main.cpp
@@ -1,4 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+//using namespace std;
+
 int main(void)
 {
+	RenderWindow win(VideoMode(800, 600), "Game");
+	Sprite spBg;
+	Texture txBg;
+	txBg.loadFromFile("res/outerspace.png");
+	spBg.setTexture(txBg);
+
+	while (win.isOpen()) {
+		Event e;
+		while (win.pollEvent(e)) {
+			if (e.type == Event::Closed) {
+				win.close();
+				break;
+			}
+		}
+		win.draw(spBg);
+		win.display();
+	}
 	return 0;
 }
