e5bd63b7b7df8eba5828bcbac3398482950f8446
Kiyong Kim <empty email>
2021-05-21 16:45:40 +0900

    거리충돌 구현.

--- cpp_18/Cpp18/MovingSprite.cpp
+++ cpp_18/Cpp18/MovingSprite.cpp
@@ -24,7 +24,25 @@ bool MovingSprite::collides(MovingSprite& sprite)
 {
 	// box
 	// distance
-	
+	Vector2f myCenter = getPosition();
+	myCenter.x += size.x / 2;
+	myCenter.y += size.y / 2;
+	float myRadius = size.x / 2.0f;
+
+	Vector2f otherCenter = sprite.getPosition();
+	Vector2u otherSize = sprite.size;
+	otherCenter.x += otherSize.x / 2;
+	otherCenter.y += otherSize.y / 2;
+	float otherRadius = otherSize.x / 2.0f;
+
+	float dx = myCenter.x - otherCenter.x;
+	float dy = myCenter.y - otherCenter.y;
+	double dist_sq = (double)dx * dx + (double)dy * dy;
+	double radius_sum = (double)myRadius + otherRadius;
+	if (sqrt(dist_sq) < radius_sum) {
+		return true;
+	}
+
 	return false;
 }
 
